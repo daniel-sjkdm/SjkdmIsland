1f4b7c891f52bad522fac0ff8cc7947af57d2e22
DanSjukdom <daniel.py.216@gmail.com>
2020-09-02 17:44:17 -0500

    Home page

---
+++ sjkdm_island/src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+    Image
+} from 'react-bootstrap';
+// import ProjectCard from './ProjectCard';
+
+
+
+const Home = (props) => {
+    return (
+        <div>
+            <Image src={require("../assets/img/Background.jpg")} fluid/>
+
+        </div>
+    );
+}
+
+export default Home;
