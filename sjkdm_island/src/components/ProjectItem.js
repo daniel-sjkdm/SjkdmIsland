6df25ac854181bf1a8c859ecdb9cb87167837758
DanSjukdom <daniel.py.216@gmail.com>
2020-09-03 18:47:39 -0500

    Like small terminals (:

--- sjkdm_island/src/components/ProjectItem.js
+++ sjkdm_island/src/components/ProjectItem.js
@@ -3,60 +3,71 @@ import {
     Card,
     CardHeader,
     CardContent,
-    Paper,
     Button,
     Typography,
-    Badge,
-    CardActions
+    CardActions,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 
+
 const useStyles = makeStyles((theme) => ({
-    Card: {
-        width: "400px",
-        height: "350px"   
+    card: {
+        height: "350px",
+        borderRadius: "30px",
+        boxShadow: "-30px 20px 20px rgba(0, 0, 0, 0.3)",
+        border: "1px solid #4DD779",
+        backgroundColor: "#121414",
+        "&hover": {
+            backgroundColor: "red"
+        }
     },
-    CardHeader: {
+    cardHeader: {
+        fontFamily: "Space Mono, mono space",
         textAlign: "center",
-        backgroundColor: "white"
+        backgroundColor: "#222222",
+        color: "white"
+    },
+    cardBody: {
+        color: "white",
+        fontFamily: "Space Mono, mono space",
+        textAlign: "justify"
     }
 }));
+
 
 const ProjectItem = ({title, description, link, tags}) => {
     const classes = useStyles();
     return (
         <div>
-            <Card className={classes.Card}>
-                <CardHeader title={title} className={classes.CardHeader}/>
+            <Card className={classes.card}>
+                <CardHeader 
+                    disableTypography={true} 
+                    title={<Typography style={{
+                        fontFamily: "Space Mono, mono space"
+                    }}> { title } </Typography>}
+                    className={classes.cardHeader}/>
                 <CardContent>
-                    <Typography 
-                        variant="body2"
-                        styles={{
-                            textAlign: "justify"
-                        }}> { description } </Typography>
-                <p> Tags: </p>
-                <div>
-                    {
+                    <Typography variant="body2" className={classes.cardBody}> 
+                        <p style={{color: "#4DD779"}}> sjukdom@castle $ ./project.sh   </p> { description } 
+                        <br/>
+                        <br/>
+                        Tags:
+                        <br/>
+                        <br/> 
+                    { 
                         tags.map((tag, index) => (
-                            <Typography
-                                variant="body2"
-                                component="p"
-                                key={index}
-                                style={{ 
-                                    marginRight: "10px",
-                                    marginLeft: "10px",
-                                }}>
-                                { tag }
-                            </Typography>
-                        ))
+                            index === tags.length-1? ( 
+                                tag 
+                            ) : tag + ", "
+                        )) 
                     }
-                </div>
+                    </Typography>
                 </CardContent>
                 <CardActions>
                     <Button 
                         size="small"
                         variant="contained" 
-                        color="primary" 
+                        color="inherit"
                         href={link}
                     > View </Button>
                 </CardActions>
@@ -67,3 +78,5 @@ const ProjectItem = ({title, description, link, tags}) => {
 
 
 export default ProjectItem;
+
+//Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
