19e3a2f81f6bf721902c2e6400eb9d1c78568796
DanSjukdom <daniel.py.216@gmail.com>
2020-09-05 19:45:11 -0500

    A key was added 4 each child component

--- sjkdm_island/src/components/SkillItem.js
+++ sjkdm_island/src/components/SkillItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Paper, List, ListItem, ListItemText, ListItemAvatar, Avatar, Typography, Icon } from '@material-ui/core';
+import { Paper, List, ListItem, ListItemText, ListItemAvatar, Avatar, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import StarIcon from '@material-ui/icons/Star';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
@@ -9,15 +9,16 @@ const useStyles = makeStyles({
     title: {
         fontFamily: "Sans Serif",
         textAlign: "center",
-        color: "white"
+        color: "white",
+        paddingTop: "20px"
     },
     item: {
-        color: "white", 
+        color: "#4DD779", 
         fontFamily: "Space Mono, mono space"
     },
     paper: {
         height: "380px",
-        borderRadius: "3px",
+        borderRadius: "10px",
         backgroundColor: "#121414",
         marginTop: "20px"
     }
@@ -39,7 +40,7 @@ const SkillItem = ({skills}) => {
                                     skill.img? (
                                         <ListItemAvatar>
                                             <Avatar>
-                                                <img src={skill.img} />
+                                                <img src={skill.img} alt=""/>
                                             </Avatar>
                                         </ListItemAvatar>
                                     ) : "" 
@@ -47,10 +48,10 @@ const SkillItem = ({skills}) => {
                                 <ListItemText primary={<Typography className={classes.item}> {skill.skill} </Typography>} disableTypography={true} />
                                 {
                                     skill.level? ( 
-                                        [1, 2, 3, 4, 5].map(i => (
-                                            i <= skill.level ? (
-                                                <StarIcon style={{ color: "white" }} />
-                                            ): <StarBorderIcon style={{ color: "white" }} />
+                                        [1, 2, 3, 4, 5].map((n, index) => (
+                                            n <= skill.level ? (
+                                                <StarIcon key={index} style={{ color: "#4DD779" }} />
+                                            ): <StarBorderIcon key={index} style={{ color: "#4DD779" }} />
                                         ))
                                     ) : ""
                                 }
