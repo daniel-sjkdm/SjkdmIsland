4495a1fd08812f008c6d98417b88bb6424d95f71
DanSjukdom <daniel.py.216@gmail.com>
2020-09-05 19:47:17 -0500

    Ordering the imports

--- sjkdm_island/src/components/Navigation.js
+++ sjkdm_island/src/components/Navigation.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import {
-    Link
-} from 'react-router-dom';
-import {
-    AppBar,
-    Toolbar,
-    Typography,
-    IconButton
-} from '@material-ui/core';
-import {
-    MenuIcon
-} from '@material-ui/icons/Menu';
-import {
-    makeStyles
-} from '@material-ui/styles';
+import { Link } from 'react-router-dom';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
 import '../assets/css/Navigation.css';
 
-
 const useStyles = makeStyles(theme => ({
-    // offset: theme.mixins.toolbar
     root: {
         marginBottom: "40px",
         backgroundColor: "#121414"
@@ -46,8 +32,12 @@ const Navigation = (props) => {
                         variant="h5"
                         className={classes.title}
                     > Sjukdom Castle </Typography>
-                
                 <div style={{display: "flex"}}>
+                    <Link to="/">
+                        <Typography className={classes.link}>
+                            Home
+                        </Typography>
+                    </Link>
                     <Link to="/projects">
                         <Typography className={classes.link}>
                             Projects
