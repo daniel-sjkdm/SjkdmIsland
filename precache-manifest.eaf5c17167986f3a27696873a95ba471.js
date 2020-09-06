1495dc50476f03a2167e4467e92bdc12cde2bed8
DanSjukdom <daniel.py.216@gmail.com>
2020-09-05 23:39:39 -0500

    Updates

---
+++ precache-manifest.eaf5c17167986f3a27696873a95ba471.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb1a8966d227c34642356093f78d82be",
+    "url": "/SjkdmIsland/index.html"
+  },
+  {
+    "revision": "87285cb523d0f2824f37",
+    "url": "/SjkdmIsland/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b5524ec4f442750fa841",
+    "url": "/SjkdmIsland/static/js/2.0f0bd473.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/SjkdmIsland/static/js/2.0f0bd473.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87285cb523d0f2824f37",
+    "url": "/SjkdmIsland/static/js/main.f64b4e04.chunk.js"
+  },
+  {
+    "revision": "9d6ee8bef77530e3b7b7",
+    "url": "/SjkdmIsland/static/js/runtime-main.fcdf479c.js"
+  },
+  {
+    "revision": "66151fee927e6e89e6879ea197313dc8",
+    "url": "/SjkdmIsland/static/media/cultures.66151fee.svg"
+  },
+  {
+    "revision": "d9c364064d2b0b6d2b50e589b3b78d52",
+    "url": "/SjkdmIsland/static/media/people.d9c36406.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b2a738b9d1ffd816e0ca1748bfc1cf24.js"
+  "/SjkdmIsland/precache-manifest.eaf5c17167986f3a27696873a95ba471.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SjkdmIsland/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
