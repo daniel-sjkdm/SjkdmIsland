dcf43b4e800f754d861138a9a615919e80d5619f
DanSjukdom <daniel.py.216@gmail.com>
2020-09-05 19:45:57 -0500

    Removed the container

--- sjkdm_island/src/components/Projects.js
+++ sjkdm_island/src/components/Projects.js
@@ -1,17 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Project from './ProjectItem';
 import project_list from '../data/projects.json';
-import {
-    Container,
-    Grid,
-    Box
-} from '@material-ui/core';
-import axios from 'axios';
+import { Grid, Box } from '@material-ui/core';
+
 
 const Projects = (props) => {
 
     const [projects, setProjects] = useState([]);
-    const [test, setTest] = useState([])
     
     useEffect(() => {
         const fetchProjects = () => {
@@ -22,24 +17,22 @@ const Projects = (props) => {
     
     return (
         <div>
-            <Container>
-                <Grid container spacing={4}>
-                        {
-                            projects.map((project) => (
-                                <Grid item lg={4} md={6} sm={12} key={project.id}>
-                                    <Box>
-                                        <Project 
-                                            title = {project.title} 
-                                            description = {project.description} 
-                                            link = {project.link}
-                                            tags = {project.tags}
-                                        />
-                                    </Box>
-                                </Grid>
-                            ))
-                        }
-                </Grid>
-            </Container>
+            <Grid container spacing={4}>
+                    {
+                        projects.map((project) => (
+                            <Grid item lg={4} md={6} sm={12} key={project.id}>
+                                <Box>
+                                    <Project 
+                                        title = {project.title} 
+                                        description = {project.description} 
+                                        link = {project.link}
+                                        tags = {project.tags}
+                                    />
+                                </Box>
+                            </Grid>
+                        ))
+                    }
+            </Grid>
         </div>
     );
 }
