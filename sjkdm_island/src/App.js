3ff4770d709f163286da49773c87afd2570f64cf
DanSjukdom <daniel.py.216@gmail.com>
2020-09-05 19:47:31 -0500

    Improving things

--- sjkdm_island/src/App.js
+++ sjkdm_island/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './assets/css/App.css';
 import {
   BrowserRouter,
   Route,
@@ -9,18 +8,32 @@ import Navigation from './components/Navigation';
 import Home from './components/Home';
 import Projects from './components/Projects';
 import About from './components/About';
+import Footer from './components/Footer';
+import { makeStyles } from '@material-ui/styles';
+import { Container } from '@material-ui/core';
+
+
+const useStyles = makeStyles({
+  app: {
+    backgroundColor: "#2E3440"
+  }
+}); 
 
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
+    <div className={classes.app}>
         <BrowserRouter>
         <Navigation />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/projects" component={Projects} />
-          <Route path="/about" component={About} />
-        </Switch>
+        <Container>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/about" component={About} />
+          </Switch>
+        </Container>
+        <Footer />
         </BrowserRouter>
     </div>
   );
