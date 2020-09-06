5921406d529fdb10fd8a6d4e710f85b2436cce97
DanSjukdom <daniel.py.216@gmail.com>
2020-09-05 20:13:51 -0500

    Updates

---
+++ precache-manifest.b2a738b9d1ffd816e0ca1748bfc1cf24.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "999e669107f3e9933b06de01e4622ced",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e999f78ccac8d2689504",
+    "url": "/static/css/main.53c39b4b.chunk.css"
+  },
+  {
+    "revision": "2949a885398f7191501f",
+    "url": "/static/js/2.6e54fbdf.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/static/js/2.6e54fbdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e999f78ccac8d2689504",
+    "url": "/static/js/main.84548dc2.chunk.js"
+  },
+  {
+    "revision": "53842a1d10e54bc0fb5f",
+    "url": "/static/js/runtime-main.da7e1a0f.js"
+  },
+  {
+    "revision": "66151fee927e6e89e6879ea197313dc8",
+    "url": "/static/media/cultures.66151fee.svg"
+  },
+  {
+    "revision": "d9c364064d2b0b6d2b50e589b3b78d52",
+    "url": "/static/media/people.d9c36406.svg"
+  }
+]);
