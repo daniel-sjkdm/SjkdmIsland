05a60c4eee8f9ebd55c1c057f48fc49412d96fbe
DanSjukdom <daniel.py.216@gmail.com>
2020-09-05 19:46:48 -0500

    Improved the css

--- sjkdm_island/src/components/Home.js
+++ sjkdm_island/src/components/Home.js
@@ -1,46 +1,49 @@
 import React from 'react';
-import { Typography, Grid } from '@material-ui/core';
+import { Typography, Grid, Container } from '@material-ui/core';
 import SkillItem from './SkillItem';
 import { makeStyles } from '@material-ui/styles';
-import castle from '../assets/img/cultures.svg';
-import background from '../assets/img/backgroun2.jpg';
 import techSkills from '../data/techSkills.json';
-import softSkills from '../data/softSkills.json';
 import interestSkills from '../data/interests.json';
-
+import CastleIcon from './CastleIcon';
 
 
 const useStyles = makeStyles({
-    root: {
-        // backgroundColor: "#212325",
-        height: "100vh"
-    },
     body: {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
         alignContent: "space-around",
-        flexDirection: "column"
+        flexDirection: "column",
+        backgroundColor: "#121414",
+        borderRadius: "10px",
+        paddingTop: "20px",
+        paddingLeft: "15px",
+        paddingRight: "15px",
     },
     title: {
-        // fontFamily: "Libre Baskerville, serif",
-        fontFamily: "Catamaran', sans-serif",
+        fontFamily: "Libre Baskerville, serif",
         fontSize: "70px",
-        color: "black",
+        color: "white",
         textAlign: "center",
-        marginBottom: "10px"
+        marginBottom: "30px",
     },
     subtitle: {
-        fontFamily: "Catamaran', sans-serif",
-        color: "black",
+        fontFamily: "Libre Baskerville, serif",
+        fontSize: "20px",
+        color: "white",
         textAlign: "center",
-        marginBottom: "20px"
+        marginBottom: "50px"
     },
     description: {
-        fontFamily: "Catamaran', sans-serif",
-        fontSize: "23px",
-        color: "black",
-        textAlign: "center"
+        fontFamily: "Libre Baskerville, serif",
+        fontSize: "18px",
+        color: "white",
+        textAlign: "center",
+        marginTop: "50px",
+        marginBottom: "50px"
+    },
+    castleLogo: {
+
     }
 });
 
@@ -49,33 +52,33 @@ const useStyles = makeStyles({
 const Home = (props) => {
     const classes = useStyles();
     return (
-        <div className={classes.root}>
-            <div className={classes.body}>
-                <Typography variant="h1" className={classes.title}>
-                    Welcome to my castle
-                </Typography>
-                <Typography variant="h5" className={classes.subtitle}>
-                    The beginning of a long journey has just started!
-                </Typography>
-                <img src={castle} width={150} height={150} />
-                <Typography variant="body2" className={classes.description} >
-                    I'm a software developer from Mexico interested in learning  <br/>
-                    and understanding new technologies and improving my skills   <br/>
-                    to keep building this castle the best way I can.
-                </Typography>
+        <Container>
+            <div>
+                <div className={classes.body}>
+                    <Typography variant="h1" className={classes.title}>
+                        Welcome to my castle
+                    </Typography>
+                    <Typography variant="h5" className={classes.subtitle}>
+                        The beginning of a long journey has just started!
+                    </Typography>
+                    <CastleIcon />
+                    <Typography variant="body2" className={classes.description} >
+                        I'm a 25 years old software developer from Mexico interested in learning     <br/>
+                        and understanding new technologies and improving my skills                   <br/>
+                        to keep building this castle the best way I can.                             <br/>
+                    </Typography>
+
+                </div>
+                <Grid container spacing={3}>
+                    <Grid item lg={6} md={6} xs={12}>
+                        <SkillItem skills={interestSkills} />
+                    </Grid>
+                    <Grid item lg={6} md={6} xs={12}>
+                        <SkillItem skills={techSkills} />
+                    </Grid>
+                </Grid>
             </div>
-            <Grid container spacing={3}>
-                <Grid item lg={4} md={6} sm={12}>
-                    <SkillItem skills={interestSkills} />
-                </Grid>
-                <Grid item lg={4} md={6} sm={12}>
-                    <SkillItem skills={softSkills} />
-                </Grid>
-                <Grid item lg={4} md={12} sm={12}>
-                    <SkillItem skills={techSkills} />
-                </Grid>
-            </Grid>
-        </div>
+        </Container>
     );
 }
 
