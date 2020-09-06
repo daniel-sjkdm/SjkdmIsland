8586ac08a5c51ef609b3314680793273054d69dc
DanSjukdom <daniel.py.216@gmail.com>
2020-09-05 19:47:46 -0500

    Main icon

---
+++ sjkdm_island/src/components/CastleIcon.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import castle from '../assets/img/cultures.svg';
+
+
+const CastleIcon = (props) => {
+    return (
+        <div>   
+            <img src={castle} width={150} height={150} alt=""/>
+        </div>
+    );
+}
+
+export default CastleIcon;
