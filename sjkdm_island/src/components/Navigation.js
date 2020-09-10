d92f38f231b773a36604dc8c64b6a21d50e7810c
daniel-sjkdm <daniel.py.216@gmail.com>
2020-09-10 17:13:07 -0500

    Improved navigation bar

--- sjkdm_island/src/components/Navigation.js
+++ sjkdm_island/src/components/Navigation.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
-import { makeStyles } from '@material-ui/styles';
+import { AppBar, Toolbar, Typography, IconButton, Drawer, Hidden } from '@material-ui/core';
+import { makeStyles, withStyles } from '@material-ui/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import SideBarList from './SideBarList';
+
 
 
 const useStyles = makeStyles(theme => ({
@@ -17,40 +20,66 @@ const useStyles = makeStyles(theme => ({
     link: {
         paddingRight: "15px",
         paddingLeft: "15px",
-        color: "white"
-    }
+        color: "white",
+        textDecoration: "none",
+    },
 }));
+
+
+const CustomDrawer = withStyles({
+    root: {
+        backgroundColor: "rgba(238,238,238,0.4)"
+    },
+    paper: {
+        backgroundColor: "#121414",
+        width: "200px",
+    }
+})(Drawer);
 
 
 const Navigation = (props) => {
     const classes = useStyles();
+    const [open, setOpen] = useState(false);    
+
+    const toggleDrawer = (open) => event => {
+        if (event.type === 'keydown' && (event.key === 'Shift' || event.key === 'Tab')) {
+            return;
+        }
+        setOpen(open)
+    }
+
     return (
         <div>
+            <CustomDrawer
+                anchor="left" 
+                open={open} 
+                onClose={toggleDrawer(false)}
+            >
+                <SideBarList handleClick={toggleDrawer}/>
+            </CustomDrawer>
             <AppBar position="static" className={classes.root}>
                 <Toolbar>
-                    <Typography 
-                        className={classes.title}
-                    > Sjukdom Castle </Typography>
+                    <Typography className={classes.title}> Sjukdom Castle </Typography>
                 <div style={{display: "flex"}}>
-                    <Link to="/SjukdomCastle">
-                        <Typography className={classes.link}>
-                            Home
-                        </Typography>
-                    </Link>
-                    <Link to="/SjukdomCastle/projects">
-                        <Typography className={classes.link}>
-                            Projects
-                        </Typography>
-                    </Link>
-                    <Link to="/SjukdomCastle/about">
-                        <Typography className={classes.link}>
-                            About
-                        </Typography>
-                    </Link>
+                    <Hidden smDown>
+                        <Link to="/SjukdomCastle" className={classes.link}>
+                            <Typography> Home </Typography>
+                        </Link>
+                        <Link to="/SjukdomCastle/projects" className={classes.link}>
+                            <Typography> Projects </Typography>
+                        </Link>
+                        <Link to="/SjukdomCastle/about" className={classes.link}> 
+                            <Typography> About </Typography>
+                        </Link>
+                    </Hidden>
+                    <Hidden smUp>
+                        <IconButton onClick={toggleDrawer(true)}>
+                            <MenuIcon color="primary"/>
+                        </IconButton>
+                    </Hidden>
                 </div>
                 </Toolbar>
             </AppBar>
-            <div className={classes.offset} />
         </div>
     );
 }
